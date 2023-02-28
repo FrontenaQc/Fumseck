b45c36ac679550851da79a7208eaa54c379c0777
dena2504 <anthony.denis@usherbrooke.ca>
2023-02-28 17:38:56 -0500

    Fumseck

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * auteur: Anthony Denis - dena2504
+ * Cours: IFT785 - H23
+ * Remise: Projet - Partie 1
+ * Remis à Mr. Najjar
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Fumseck - Systême de paie");
+    }
+}
